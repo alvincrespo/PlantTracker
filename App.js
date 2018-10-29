094a51c2fd76fe4f71411ade0467f913c0d9c680
Alvin Crespo <alvin.crespo@gmail.com>
2018-10-29 10:25:32 -0400

    Setup emotion for styling

--- App.js
+++ App.js
@@ -7,42 +7,34 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import styled from "@emotion/primitives";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu"
-});
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5fcff;
+`;
 
-type Props = {};
-export default class App extends Component<Props> {
+const Title = styled.Text`
+  font-size: 20px;
+  text-align: center;
+  color: hotpink;
+`;
+
+const Description = styled.Text`
+  text-align: center;
+  color: #333333;
+  margin-bottom: 5px;
+`;
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Title>Welcome to Emotion Primitives</Title>
+        <Description>Style and render primitives across targets.</Description>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
